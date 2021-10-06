25c0b5268bf1f930fc921c51d2e78b0d743cafcf
Sanusi Cole <sanusi1612@icloud.com>
2021-10-06 15:31:36 -0400

    added routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,165 @@
+const router = require('express').Router();
+const { Article } = require('../../models');
+const withAuth = require('../../utils/auth');
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newArticle = await Article.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newArticle);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const articleData = await Article.update(
+    {
+      title: req.body.articleTitle,
+      content: req.body.articleContent,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!articleData) {
+      res.status(404).json({ message: 'No article found with this id.' });
+      return;
+    }
+    res.status(200).json(articleData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const articleData = await Article.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!articleData) {
+      res.status(404).json({ message: 'No item(s) found with this id!' });
+      return;
+    }
+    res.status(200).json(articleData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
+
+articleRoutes.js
+commentRoutes
+
+
+const { Comment, Article, User} = require('../../models');
+const withAuth = require('../../utils/auth');
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+        {
+          model: Article,
+          attributes: ['username'],
+        },
+      ],
+      where: {
+          article_id: req.params.id,
+        },
+      order: [['created_at', 'ASC']],
+    });
+    const comments = commentData.map((comment) => comment.get({ plain: true }));
+    res.render('comments', { 
+      comments, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.findOne({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(newComment);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.update(
+    {
+      content: req.body.commentContent,
+    },
+    {
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id.' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.error(err);
+    res.status(400).json(err);
+  }
+});
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+    if (!commentData) {
+      res.status(404).json({ message: 'No comment found with this id!' });
+      return;
+    }
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+module.exports = router;
+
+
+
+
+
